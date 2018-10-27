5ae51848a51cc35b2e3bfd6cfc3c2eba2f19c92d
Giulio Carducci <carduc.92@gmail.com>
2018-10-27 21:32:21 +0200

    cambiate foto

--- images/foto/resize.py
+++ images/foto/resize.py
@@ -1,13 +1,14 @@
 from PIL import Image
 import os
 
-percent = 1
+percent = 0.4
 
 for file_name in os.listdir("../foto/"):
-	print(file_name)
-	img = Image.open("../foto/"+str(file_name))
-	hsize = int((float(img.size[0]) * float(percent)))
-	vsize = int((float(img.size[1]) * float(percent)))
-	img = img.resize((hsize, vsize), Image.ANTIALIAS)
-	img.save(file_name)
+	if file_name == "pic8.jpg":
+		print(file_name)
+		img = Image.open("../foto/"+str(file_name))
+		hsize = int((float(img.size[0]) * float(percent)))
+		vsize = int((float(img.size[1]) * float(percent)))
+		img = img.resize((hsize, vsize), Image.ANTIALIAS)
+		img.save(file_name)
 		
--- images/thumbs/resize.py
+++ images/thumbs/resize.py
@@ -4,16 +4,17 @@ import os
 percent = 0.5
 
 for file_name in os.listdir("../foto/"):
-	img = Image.open("../foto/"+str(file_name))
+	if file_name == "pic8.jpg":
+		img = Image.open("../foto/"+str(file_name))
 
-	if img.size[0] > img.size[1]:
-		#foto orizzontale
-		hsize = int((float(img.size[0]) * float(percent)))
-		vsize = int((float(img.size[1]) * float(percent)))
-	else:
-		#foto verticale
-		hsize = int((float(img.size[0]) * float(percent)))
-		vsize = int((float(img.size[1]) * float(percent)))
-	img = img.resize((hsize, vsize), Image.ANTIALIAS)
-	img.save(file_name)
+		if img.size[0] > img.size[1]:
+			#foto orizzontale
+			hsize = int((float(img.size[0]) * float(percent)))
+			vsize = int((float(img.size[1]) * float(percent)))
+		else:
+			#foto verticale
+			hsize = int((float(img.size[0]) * float(percent)))
+			vsize = int((float(img.size[1]) * float(percent)))
+		img = img.resize((hsize, vsize), Image.ANTIALIAS)
+		img.save(file_name)
 		
