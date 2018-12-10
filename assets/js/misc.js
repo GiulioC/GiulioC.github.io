3f1d6d9e51b54d4d6f1345c98229fe5737abe96a
Giulio Carducci <carduc.92@gmail.com>
2018-12-10 11:39:31 +0100

    added other skills subsection

--- assets/js/misc.js
+++ assets/js/misc.js
@@ -1,3 +1,14 @@
 $('#foto-avatar').click(function(e) {
 	console.log("Ciao !")
 })
+
+$('#all-skills-show').click(function(e) {
+	$('#all-skills-show').hide()
+		$('#all-skills').slideDown()
+})
+
+$('#all-skills-hide').click(function(e) {
+	$('#all-skills').slideUp(function() {
+		$('#all-skills-show').fadeIn()
+	})
+})
